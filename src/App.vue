2dc3958201f9b438dc91d56e750cf9be7cea0d9b
Alex <storyday2012@gmail.com>
2020-11-30 22:41:37 +0100

    work on search fields

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <div id="nav">
-   
-  </div>
+  <div id="nav"></div>
   <router-view />
 </template>
 
 <style lang="scss">
-@import '~materialize-css/dist/css/materialize.min.css';
-@import 'assets/css/main.min.css';
-@import 'assets/css/normalize.css';
-@import 'assets/css/style.css';
-@import 'assets/css/slick.css'
-
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/css/main.min.css";
+@import "assets/css/normalize.css";
+@import "assets/css/style.css";
+@import "assets/css/slick.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+
+import { VuelidatePlugin } from '@vuelidate/core'
+import "materialize-css/dist/js/materialize.min.js";
+
+
+
+ 
+
 createApp(App)
   .use(store)
+  .use(VuelidatePlugin)
   .use(router)
   .mount("#app");
