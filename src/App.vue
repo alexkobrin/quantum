3d6e63ca49558d6fa83d4e0869ced4ae6c33b767
Alex <storyday2012@gmail.com>
2020-11-30 13:36:09 +0100

    added styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+   
   </div>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/css/main.min.css';
+@import 'assets/css/normalize.css';
+@import 'assets/css/style.css';
+@import 'assets/css/slick.css'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
